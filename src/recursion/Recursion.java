22268f4fac672b179016fb11b490887bdf6234c5
Unknown <Aidan.S.Edwards@gmail.com>
2018-01-31 15:44:12 -0600

    Implemented isSubsetSum

--- src/recursion/Recursion.java
+++ src/recursion/Recursion.java
@@ -1,6 +1,7 @@
 package recursion;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Recursion {
@@ -8,12 +9,17 @@ public class Recursion {
 	public static void main(String[] args) {
 		List<Integer> randomMultiSet = randomIntegerGenerator(new ArrayList<Integer>(), 5);
 
-		// TODO: Print the list above using your printList method
+		// DONE: Print the list above using your printList method
         printList(randomMultiSet);
 		System.out.println();
 		
-		// TODO: Print the results of running your isSubsetSum method on the above list
+		// DONE: Print the results of running your isSubsetSum method on the above list
 		System.out.println(isSubsetSum(randomMultiSet));
+
+		/*(List<Integer> testMultiSet = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, -4));
+		printList(testMultiSet);
+		System.out.println();
+		System.out.println(isSubsetSum(testMultiSet));*/
 	}
 
 	public static List<Integer> randomIntegerGenerator(List<Integer> generate, int length) {
@@ -45,10 +51,23 @@ public class Recursion {
 	    printList(list, 0);
     }
 
-	// TODO: Write a static method isSubsetSum which checks a list to see if there is a
+	// Done: Write a static method isSubsetSum which checks a list to see if there is a
 	// non-empty multiset(set with repeats allowed so basically a list) which sums
 	// to zero and returns a boolean without any loops
 	public static boolean isSubsetSum(List<Integer> list){
-		return false;
+		return isSubsetSum(list, list.size() - 1, 0, true);
+	}
+
+	private static boolean isSubsetSum(List<Integer> list, int index, int sum, boolean empty){
+		if(index == -1){
+			return !empty && sum == 0;
+		}else{
+			boolean with = isSubsetSum(list, index-1, sum + list.get(index), false);
+			boolean without = isSubsetSum(list, index-1, sum, empty);
+			/*if(with){
+				System.out.print("[" + list.get(index) + " " + sum + " " + (sum + list.get(index)) + "]");
+			}*/
+			return  with || without;
+		}
 	}
 }
